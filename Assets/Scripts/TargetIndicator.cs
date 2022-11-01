e73dbc20eeb41bb8d025da73557d3b043770cb08
F-Alex-projects <alex@franquez.com>
2022-11-01 14:47:27 -0400

    attempt at script changes

--- Assets/Scripts/TargetIndicator.cs
+++ Assets/Scripts/TargetIndicator.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class TargetIndicator : MonoBehaviour
 {
-    [SerializeField] GameObject Target;
+    [SerializeField] Transform Target;
     [SerializeField] GameObject Player;
     
     void Update()
     {
         if (!Player.GetComponent<PlayerMovement>().hasDelivery)
         {
-            Target = GameObject.FindGameObjectWithTag("PickupPoint");
+            //Target = GameObject.FindGameObjectWithTag("PickupPoint");
         }
         else if(Player.GetComponent<PlayerMovement>().hasDelivery)
         {
-            Target = GameObject.FindGameObjectWithTag("DropoffPoint");
+            //Target = GameObject.FindGameObjectWithTag("DropoffPoint");
         }
         else
         {
-            Target = GameObject.FindGameObjectWithTag("PickupPoint");
+            //Target = GameObject.FindGameObjectWithTag("PickupPoint");
         }
-        var direction = Target.transform.position - transform.position;
+        var direction = Target.position - transform.position;
 
         var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
